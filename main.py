f86ed04d47d464d6a5eae46b7a0a4a182b99a4f2
Aditya Pandey <89931465+adityapandey343@users.noreply.github.com>
2023-08-22 18:02:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def header(url):
 
 header("Find Me A Friend")
 
-with open( "css\style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+#with open( "css\style.css" ) as css:
+    #st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
 
 
